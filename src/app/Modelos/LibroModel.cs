7dcb3f8c5db71d3bcb2c1159e2afde679d3d5096
DaamiAle <daamiale.recoil@gmail.com>
2022-07-15 14:03:57 -0300

    Se agregaron: Dependencias SQLITE y EntityFrameworkCore. Modelo Libro.

---
+++ src/app/Modelos/LibroModel.cs
@@ -0,0 +1,22 @@
+﻿
+namespace BibliotecaQuarkAcademy.src.app.Modelos
+{
+    public class LibroModel
+    {
+        private int id;
+        private string nombre;
+        private string autor;
+        private string codigoISBN;
+
+
+
+        public int Id() => id;
+        public void Id(int value) => id = value;
+        public string Nombre() => nombre;
+        public void Nombre(string value) => nombre = value;
+        public string Autor() => autor;
+        public void Autor(string value) => autor = value;
+        public string CodigoISBN() => codigoISBN;
+        public void CodigoISBN(string value) => codigoISBN = value;
+    }
+}

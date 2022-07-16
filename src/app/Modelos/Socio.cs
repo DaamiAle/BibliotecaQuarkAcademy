7805c212fc03f4b5232d76e41393e0c3f5ea0f7d
DaamiAle <daamiale.recoil@gmail.com>
2022-07-16 11:27:35 -0300

    Se agrega modelo de Socio

---
+++ src/app/Modelos/Socio.cs
@@ -0,0 +1,20 @@
+﻿
+namespace BibliotecaQuarkAcademy.src.app.Modelos
+{
+    public class Socio
+    {
+        private int id;
+        private string nombre;
+        private string apellido;
+        private int numIdentificacion;
+
+        public int Id() => id;
+        public void Id(int value) => id = value;
+        public string Nombre() => nombre;
+        public void Nombre(string value) => nombre = value;
+        public string Apellido() => apellido;
+        public void Apellido(string value) => apellido = value;
+        public int NumIdentificacion() => numIdentificacion;
+        public void NumIdentificacion(int value) => numIdentificacion = value;
+    }
+}

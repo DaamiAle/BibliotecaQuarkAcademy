3f4cc173db7961eee44e1d8feafa8326f0fc9f40
DaamiAle <daamiale.recoil@gmail.com>
2022-07-16 11:32:28 -0300

    Se modifica model Socio para que contemple la posibilidad de un socio VIP (Parte C)

--- src/app/Modelos/Socio.cs
+++ src/app/Modelos/Socio.cs
@@ -3,11 +3,15 @@ namespace BibliotecaQuarkAcademy.src.app.Modelos
 {
     public class Socio
     {
+        // Atributos
         private int id;
         private string nombre;
         private string apellido;
         private int numIdentificacion;
+        private bool esVIP;
+        private int cuotaSocio = 0;
 
+        // Propiedades
         public int Id() => id;
         public void Id(int value) => id = value;
         public string Nombre() => nombre;
@@ -16,5 +20,9 @@ namespace BibliotecaQuarkAcademy.src.app.Modelos
         public void Apellido(string value) => apellido = value;
         public int NumIdentificacion() => numIdentificacion;
         public void NumIdentificacion(int value) => numIdentificacion = value;
+        public bool EsVIP() => esVIP;
+        public void EsVIP(bool value) => esVIP = value;
+        public int CuotaSocio() => cuotaSocio;
+        public void CuotaSocio(int value) => cuotaSocio = value;
     }
 }

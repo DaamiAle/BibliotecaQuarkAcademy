42b416b959045b7494b96068829c3f3dbf484592
DaamiAle <daamiale.recoil@gmail.com>
2022-07-16 11:43:49 -0300

    Se modifico modelo de libro para contemplar una lista de ejemplares disponibles.

--- src/app/Modelos/LibroModel.cs
+++ src/app/Modelos/LibroModel.cs
@@ -3,13 +3,14 @@ namespace BibliotecaQuarkAcademy.src.app.Modelos
 {
     public class LibroModel
     {
+        // Atributos
         private int id;
         private string nombre;
         private string autor;
         private string codigoISBN;
+        private List<EjemplarModel> ejemplaresDisponibles;
 
-
-
+        // Propiedades
         public int Id() => id;
         public void Id(int value) => id = value;
         public string Nombre() => nombre;
@@ -18,5 +19,7 @@ namespace BibliotecaQuarkAcademy.src.app.Modelos
         public void Autor(string value) => autor = value;
         public string CodigoISBN() => codigoISBN;
         public void CodigoISBN(string value) => codigoISBN = value;
+        public List<EjemplarModel> EjemplaresDisponibles() => ejemplaresDisponibles;
+        public void EjemplaresDisponibles(List<EjemplarModel> value) => ejemplaresDisponibles = value;
     }
 }

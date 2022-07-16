f63a235ce6e0a7f802fd59c1c3a30e16c43da64d
DaamiAle <daamiale.recoil@gmail.com>
2022-07-16 11:40:28 -0300

    Se agrega modelo de Ejemplar.

---
+++ src/app/Modelos/EjemplarModel.cs
@@ -0,0 +1,22 @@
+﻿
+namespace BibliotecaQuarkAcademy.src.app.Modelos
+{
+    public class EjemplarModel
+    {
+        // Atributos
+        private int id;
+        private LibroModel libro;
+        private int numEdicion;
+        private string ubicacion;
+
+        // Propiedades
+        public int Id() => id;
+        public void Id(int value) => id = value;
+        public LibroModel Libro() => libro;
+        public void Libro(LibroModel value) => libro = value;
+        public int NumEdicion() => numEdicion;
+        public void NumEdicion(int value) => numEdicion = value;
+        public string Ubicacion() => ubicacion;
+        public void Ubicacion(string value) => ubicacion = value;
+    }
+}
